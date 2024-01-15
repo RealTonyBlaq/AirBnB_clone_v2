48d6e96d4afdcb60f1a314baaa9de5dac125c426
RealTonyBlaq <ifeanyiikpenyi@yahoo.com>
2024-01-16 00:00:05 +0100

    buidl

--- console.py
+++ console.py
@@ -136,7 +136,6 @@ class HBNBCommand(cmd.Cmd):
                         except ValueError:
                             pass
                 kwargs[item[0]] = item[1]
-                print("{}: {} - ({})".format(item[0], item[1], type(item[1])))
         else:
             kwargs = None
         new_instance = HBNBCommand.classes[params[0]]()
