4cc3dec6858c87b64cb1a6efaecb238f156b55d9
RealTonyBlaq <ifeanyiikpenyi@yahoo.com>
2024-01-16 00:16:54 +0100

    buidl

--- console.py
+++ console.py
@@ -137,6 +137,8 @@ class HBNBCommand(cmd.Cmd):
                         except ValueError:
                             pass
                 kwargs[item[0]] = item[1]
+            kwargs['created_at'] = (datetime.now())
+            kwargs['updated_at'] = datetime.now()
         else:
             kwargs = None
         new_instance = HBNBCommand.classes[params[0]](**kwargs)
