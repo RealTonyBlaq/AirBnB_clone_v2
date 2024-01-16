edbdcff063d5e3e7c0e7df9f5101c98f83426d64
RealTonyBlaq <ifeanyiikpenyi@yahoo.com>
2024-01-16 23:34:01 +0100

    buidl

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,4 +1,5 @@
 -- Script preps a mysql server for the project
 
 CREATE DATABASE IF NOT EXISTS hbnb_dev_db
-CREATE USER IF NOT EXISTS hbnb_dev
+CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost'
+IDENTIFIED BY 
