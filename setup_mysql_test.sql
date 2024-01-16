517289ceab24069773c9d73c4b7bba3ff2ba7c94
RealTonyBlaq <ifeanyiikpenyi@yahoo.com>
2024-01-17 00:02:28 +0100

    another setup script

---
+++ setup_mysql_test.sql
@@ -0,0 +1,6 @@
+-- Script sets up a mysql server for use in this project
+
+CREATE DATABASE IF NOT EXISTS hbnb_test_db;
+CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED WITH mysql_native_password BY 'hbnb_test_pwd';
+GRANT ALL PRIVILEGES ON hbnb_test_db.* TO 'hbnb_test'@'localhost';
+GRANT SELECT ON performance_schema.* TO 'hbnb_test'@'localhost';
