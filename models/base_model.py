04776e6fcdb3ad556af46bd8948256bbdb28632c
RealTonyBlaq <ifeanyiikpenyi@yahoo.com>
2024-01-17 20:48:28 +0100

    building a db storage

--- models/base_model.py
+++ models/base_model.py
@@ -56,5 +56,6 @@ class BaseModel:
                           (str(type(self)).split('.')[-1]).split('\'')[0]})
         dictionary['created_at'] = self.created_at.isoformat()
         dictionary['updated_at'] = self.updated_at.isoformat()
-        
+        if '_sa_instance_state' in dictionary.keys():
+            
         return dictionary
