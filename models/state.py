cc583678820c14331e0258d40c5e9c956b6bc3ef
RealTonyBlaq <ifeanyiikpenyi@yahoo.com>
2024-01-17 21:20:24 +0100

    add features (db_storage)

--- models/state.py
+++ models/state.py
@@ -2,6 +2,7 @@
 """ State Module for HBNB project """
 from models.base_model import BaseModel, Base
 from sqlalchemy import String, Column
+from sqlalchemy.orm import relationship
 
 
 class State(BaseModel, Base):
@@ -11,4 +12,5 @@ class State(BaseModel, Base):
     """
     __tablename__ = 'states'
     name = Column('name', String(128), nullable=False)
-
+    cities = relationship("City", back_populates="state",
+                          cascade="all, delete")
