af1b71ac6e6bcee92b4a0558b86d252264de7a03
RealTonyBlaq <ifeanyiikpenyi@yahoo.com>
2024-01-17 20:47:09 +0100

    building a db storage

--- models/base_model.py
+++ models/base_model.py
@@ -4,7 +4,6 @@ import uuid
 from datetime import datetime
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import Column, String, DateTime
-from models import storage
 
 Base = declarative_base()
 
@@ -57,4 +56,5 @@ class BaseModel:
                           (str(type(self)).split('.')[-1]).split('\'')[0]})
         dictionary['created_at'] = self.created_at.isoformat()
         dictionary['updated_at'] = self.updated_at.isoformat()
+        
         return dictionary
