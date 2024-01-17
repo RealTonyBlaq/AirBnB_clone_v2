adbe6fd666c7a38e48032d152f2e973d1cf13854
RealTonyBlaq <ifeanyiikpenyi@yahoo.com>
2024-01-17 21:32:27 +0100

    more features, comments

--- models/city.py
+++ models/city.py
@@ -5,9 +5,9 @@ from sqlalchemy import Column, String, ForeignKey
 from sqlalchemy.orm import relationship
 
 class City(BaseModel, Base):
-    """ The city class, contains state ID and name """
+    """ Defining the city class """
     __tablename__ = "cities"
     state_id = Column("state_id", String(60), ForeignKey('states.id'),
                       nullable=False)
     name = Column("name", String(128), nullable=False)
-    states = relationship('State', back_populates='')
+    states = relationship('State', back_populates='cities')
