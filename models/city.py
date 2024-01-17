767926419f17ce5d1929cafa64921b70c07f348e
RealTonyBlaq <ifeanyiikpenyi@yahoo.com>
2024-01-17 21:04:23 +0100

    add features

--- models/city.py
+++ models/city.py
@@ -7,5 +7,6 @@ from sqlalchemy import Column, String, ForeignKey
 class City(BaseModel, Base):
     """ The city class, contains state ID and name """
     __tablename__ = 'cities'
-    state_id = Column('state_id', String(60), ForeignKey('state.id') nullable=False)
+    state_id = Column('state_id', String(60), ForeignKey('state.id'),
+                      nullable=False)
     name = ""
