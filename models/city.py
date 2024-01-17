6b501d8d8ba474a50af90e5ad7a279dc10cfbec0
RealTonyBlaq <ifeanyiikpenyi@yahoo.com>
2024-01-17 21:01:49 +0100

    add features

--- models/city.py
+++ models/city.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 """ City Module for HBNB project """
 from models.base_model import BaseModel, Base
-from sqlalchemy import Column, String
+from sqlalchemy import Column, String, ForeignKey
 
 
 class City(BaseModel, Base):
     """ The city class, contains state ID and name """
     __tablename__ = 'cities'
-    state_id = Column('state_id', String(128), )
+    state_id = Column('state_id', String(60),  nullable=False)
     name = ""
