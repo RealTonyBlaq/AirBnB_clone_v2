272772d0c339a587ac10a2ee42035562f68613fb
RealTonyBlaq <ifeanyiikpenyi@yahoo.com>
2024-01-18 20:15:02 +0100

    defining DBStorage

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -1,12 +1,13 @@
 #!/usr/bin/python3
 
 """ Module for Database storage """
-
+from sqlalchemy 
 
 class DBStorage:
     """ Defining the class DBStorage """
     __engine = None
     __session = None
 
-    def __init__(self) -> None:
-        pass
+    def __init__(self):
+        """ Initializing the attributes """
+
