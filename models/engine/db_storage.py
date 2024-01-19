d63f2cf424e50a6fc2f398fb3020ed8f7cb05988
RealTonyBlaq <ifeanyiikpenyi@yahoo.com>
2024-01-19 13:34:37 +0100

    update

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -2,6 +2,7 @@
 
 """ Module for Database storage """
 from sqlalchemy import create_engine, MetaData
+from sqlalchemy.orm import sessionmaker
 import os
 
 user = os.environ.get("HBNB_MYSQL_USER")
@@ -20,6 +21,7 @@ class DBStorage:
         self.__engine = create_engine("mysql://{}:{}@{}:3306/{}"
                                       .format(user, passwd, host, database),
                                       pool_pre_ping=True)
+        self.
         if os.environ.get("HBNB_ENV") == "test":
             metadata = MetaData(bind=self.__engine)
             metadata.reflect()
@@ -28,4 +30,4 @@ class DBStorage:
     def all(self, cls=None):
         """ Queries the current database session """
         if cls:
-            
+
