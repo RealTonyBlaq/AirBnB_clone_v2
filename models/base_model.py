d6c0d2674860598a32f016c6f263e1ad4f80b21f
RealTonyBlaq <ifeanyiikpenyi@yahoo.com>
2024-01-21 15:14:25 +0100

    fix

--- models/base_model.py
+++ models/base_model.py
@@ -20,9 +20,12 @@ class BaseModel:
     def __init__(self, *args, **kwargs):
         """Instatntiates a new model"""
         if not kwargs or kwargs is None:
+            from models import storage
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
+            storage.new(self)
+            storage.save()
         else:
             if 'updated_at' in kwargs.keys():
                 kwargs['updated_at'] = datetime.strptime(
