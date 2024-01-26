0af5fc858235a1820a798d38a5100077dd8fd445
RealTonyBlaq <ifeanyiikpenyi@yahoo.com>
2024-01-26 21:50:36 +0100

    updating Place class

--- models/place.py
+++ models/place.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
 """ Place Module for HBNB project """
 from models.base_model import BaseModel, Base
+from sqlalchemy import String, Column
 
 
-class Place(BaseModel):
-    """ A place to stay """
+class Place(BaseModel, Base):
+    """ Defining the class Place: A place to stay """
     city_id = ""
     user_id = ""
     name = ""
