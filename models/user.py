5e49ea894529090d3772a2a33775ebb075c7c4ad
RealTonyBlaq <ifeanyiikpenyi@yahoo.com>
2024-01-26 21:35:38 +0100

    updating class User

--- models/user.py
+++ models/user.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
 """This module defines a class User"""
 from models.base_model import BaseModel, Base
-from sqlalchemy import 
+from sqlalchemy import String, Column
 
 
 class User(BaseModel, Base):
     """This class defines a user by various attributes"""
     __tablename__ = "users"
-    email = ''
+    email = Column("email", String(128), null)
     password = ''
     first_name = ''
     last_name = ''
