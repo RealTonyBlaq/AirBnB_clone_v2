c6da74b289757b87e8b4f1c4e6b8ed06f0cd7fdd
RealTonyBlaq <ifeanyiikpenyi@yahoo.com>
2024-01-26 22:14:08 +0100

    update

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -13,7 +13,7 @@ from models.city import City
 from models.amenity import Amenity
 from models.review import Review
 
-classes = [State, City, User]
+classes = [State, City, User, Place]
 
 user = getenv("HBNB_MYSQL_USER")
 host = getenv("HBNB_MYSQL_HOST")
