f8d7122b7ef28a50e09b82a2c1f4c586a1ca4078
RealTonyBlaq <ifeanyiikpenyi@yahoo.com>
2024-01-27 10:09:22 +0100

    updating class Review

--- models/review.py
+++ models/review.py
@@ -6,7 +6,7 @@ from sqlalchemy.orm import relationship
 
 
 class Review(BaseModel, Base):
-    """ Review class to store review information """
+    """ Defining the Review class to store review information """
     __tablename__ = "reviews"
     place_id = Column("place_id", String(60), ForeignKey("places.id"), nullable=False)
     user_id = Column("user_id", String(60), ForeignKey("users.id"), nullable=False)
