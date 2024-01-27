b540fa5f81ea52207ce482c8b6e6d1ac456cf6be
RealTonyBlaq <ifeanyiikpenyi@yahoo.com>
2024-01-27 13:59:16 +0100

    updating attributes

--- models/amenity.py
+++ models/amenity.py
@@ -2,10 +2,11 @@
 """ State Module for HBNB project """
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
 
 
 class Amenity(BaseModel, Base):
     """ Defines the class Amenity """
     __tablename__ = "amenities"
     name = Column("name", String(128), nullable=False)
-    place_amenities
+    place_amenities = relationship("Place", )
