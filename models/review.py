64d6f24d4acdd20b4c11b6285234ef04723f0d0e
RealTonyBlaq <ifeanyiikpenyi@yahoo.com>
2024-01-27 10:02:16 +0100

    updating class Review

--- models/review.py
+++ models/review.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
 """ Review module for the HBNB project """
 from models.base_model import BaseModel, Base
-from sqlalchemy import String, Column
+from sqlalchemy import String, Column, ForeignKey
 
 
 class Review(BaseModel):
     """ Review class to store review information """
     __tablename__ = "reviews"
-    place_id = ""
+    place_id = Column("place_id", String(60), ForeignKey("places.id"))
     user_id = ""
     text = Column("text", String(1024), nullable=False)
