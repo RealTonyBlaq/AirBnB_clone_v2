7e59a7cf225b93b1d5460ae3a5017b7f6f7c5980
RealTonyBlaq <ifeanyiikpenyi@yahoo.com>
2024-01-27 10:06:08 +0100

    updating class Review

--- models/review.py
+++ models/review.py
@@ -11,3 +11,4 @@ class Review(BaseModel, Base):
     place_id = Column("place_id", String(60), ForeignKey("places.id"), nullable=False)
     user_id = Column("user_id", String(60), ForeignKey("users.id"), nullable=False)
     text = Column("text", String(1024), nullable=False)
+    users = relationship("User", )
