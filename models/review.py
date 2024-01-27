4b2bd85d6f27a494c10fe7fbab1366fa1ca0aa4d
RealTonyBlaq <ifeanyiikpenyi@yahoo.com>
2024-01-27 10:03:33 +0100

    updating class Review

--- models/review.py
+++ models/review.py
@@ -4,9 +4,9 @@ from models.base_model import BaseModel, Base
 from sqlalchemy import String, Column, ForeignKey
 
 
-class Review(BaseModel):
+class Review(BaseModel, Base):
     """ Review class to store review information """
     __tablename__ = "reviews"
-    place_id = Column("place_id", String(60), ForeignKey("places.id"))
-    user_id = ""
+    place_id = Column("place_id", String(60), ForeignKey("places.id"), nullable=False)
+    user_id = Column("user_id", String(60), ForeignKey("users.id"), nullable=False)
     text = Column("text", String(1024), nullable=False)
