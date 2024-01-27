ff3879b480229b631e8c356e585f1a4fe1b698ef
RealTonyBlaq <ifeanyiikpenyi@yahoo.com>
2024-01-27 10:00:59 +0100

    updating class Review

--- models/review.py
+++ models/review.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """ Review module for the HBNB project """
 from models.base_model import BaseModel, Base
+from sqlalchemy import String, Column
 
 
 class Review(BaseModel):
@@ -8,4 +9,4 @@ class Review(BaseModel):
     __tablename__ = "reviews"
     place_id = ""
     user_id = ""
-    text = ""
+    text = Column("text", String(1024), nullable=False)
