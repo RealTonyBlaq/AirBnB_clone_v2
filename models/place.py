c5a2589ffa9b0c72361aab0fb6a6834dc20c924e
RealTonyBlaq <ifeanyiikpenyi@yahoo.com>
2024-01-27 14:28:56 +0100

    updating place to use many-to-many relationship

--- models/place.py
+++ models/place.py
@@ -1,9 +1,14 @@
 #!/usr/bin/python3
 """ Place Module for HBNB project """
 from models.base_model import BaseModel, Base
-from sqlalchemy import String, Column, ForeignKey, Integer, Float
+from sqlalchemy import (String, Column, ForeignKey, Integer, Float,
+                        Table)
 from sqlalchemy.orm import relationship
 
+metadata = Base.metadata()
+
+place_amenity = Table("place_amenity", metadata,
+                      Column("place_id", String))
 
 class Place(BaseModel, Base):
     """ Defining the class Place:
