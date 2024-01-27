9495d01b311454d9db39f7ad255783009a827f3f
RealTonyBlaq <ifeanyiikpenyi@yahoo.com>
2024-01-27 09:48:21 +0100

    update

--- models/user.py
+++ models/user.py
@@ -12,4 +12,4 @@ class User(BaseModel, Base):
     password = Column("password", String(128), nullable=False)
     first_name = Column("first_name", String(128))
     last_name = Column("last_name", String(128))
-    places = relationship("Place", backref="user", cascade="all, delete")
+    places = relationship("Place", back_populates="users", cascade="all, delete")
