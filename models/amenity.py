71415d5aba39de94bc8f1f09bebf4e12f6a0763a
RealTonyBlaq <ifeanyiikpenyi@yahoo.com>
2024-01-27 14:58:29 +0100

    imported the secondary table to be used as a m-m relationship to Place

--- models/amenity.py
+++ models/amenity.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
 from models.base_model import BaseModel, Base
+from models.place import place_amenity
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
 
@@ -9,4 +10,4 @@ class Amenity(BaseModel, Base):
     """ Defines the class Amenity """
     __tablename__ = "amenities"
     name = Column("name", String(128), nullable=False)
-    place_amenities = relationship("Place", )
+    place_amenities = relationship("Place", secondary=place_amenity, back_populates="amenities")
