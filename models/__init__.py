d88e491ce7586a9196c2cecd3103c97911dea3dd
RealTonyBlaq <ifeanyiikpenyi@yahoo.com>
2024-01-28 14:35:34 +0100

    update

--- models/__init__.py
+++ models/__init__.py
@@ -1,5 +1,11 @@
 #!/usr/bin/python3
 """This module instantiates an object of the storage system in use"""
+from models.user import User
+from models.place import Place
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.review import Review
 from os import getenv
 
 if getenv("HBNB_TYPE_STORAGE") == "db":
