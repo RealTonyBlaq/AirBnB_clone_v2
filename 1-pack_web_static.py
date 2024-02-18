93111b59d73cef856b60e3a736e00bff13c39f15
RealTonyBlaq <ifeanyiikpenyi@yahoo.com>
2024-02-18 17:58:05 +0100

    fabric

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
 """ Script generates a .tgz archive using fabric """
 
-from fabric import Connection
+from fabric import task
 
 
+
+@task
 def do_pack(cc):
 	"""
 	do_pack - Fab function to generate .tgz archives
