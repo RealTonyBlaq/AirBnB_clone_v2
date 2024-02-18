fedb389b5f44c8b745e2e9de9782cb8773b44207
RealTonyBlaq <ifeanyiikpenyi@yahoo.com>
2024-02-18 18:49:12 +0100

    fix

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,16 +1,16 @@
 #!/usr/bin/python3
 """ Script generates a .tgz archive using fabric """
 
-from fabric import task
+from fabric.api import local, cd
 
 
 @task
 def do_pack(cc):
-	"""
-	do_pack - Fab function to generate .tgz archives
-	"""
-	with cc.cd('~/AirBnB_clone_v2'):
-		result = cc.local('date +"%Y%m%d%H%M%S"')
-		cc.local('tar -czvf web_static{}.tgz ./web_static/'.format(result))
-		cc.local('mkdir -p versions/')
-		cc.local('mv web_static{}.tgz versions/'.format(result))
+    """
+    do_pack - Fab function to generate .tgz archives
+    """
+    with cd('~/AirBnB_clone_v2'):
+        result = local('date +"%Y%m%d%H%M%S"')
+		local('tar -czvf web_static{}.tgz ./web_static/'.format(result))
+        local('mkdir -p versions/')
+        local('mv web_static{}.tgz versions/'.format(result))
