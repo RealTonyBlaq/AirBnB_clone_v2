77386499601ca8adcfd4ed3c915a76258f9cbb6e
RealTonyBlaq <ifeanyiikpenyi@yahoo.com>
2024-02-18 19:16:55 +0100

    date fix

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -2,13 +2,15 @@
 """ Script generates a .tgz archive using fabric """
 
 from fabric.api import local
+from datetime import datetime
 
 
 def do_pack():
     """
     do_pack - Fab function to generate .tgz archives
     """
-    arch = "web_static_{}.tgz".format(local('date +"%Y%m%d%H%M%S"'))
+    time = datetime.utcnow().strftime("%Y%m%d%H%M%S")
+    arch = "web_static_{}.tgz".format(time)
     local('mkdir -p versions/')
     archive = local('tar -czvf versions/{} web_static'.format(arch))
     if archive.failed:
