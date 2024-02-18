b830b7cd93326f856bb473fddc67f809bdb73094
RealTonyBlaq <ifeanyiikpenyi@yahoo.com>
2024-02-18 12:40:56 +0100

    set up web_static

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -15,4 +15,5 @@ sudo mkdir -p /data/web_static/releases/test/
 sudo echo "Testing my Nginx congiguration" > /data/web_static/releases/test/index.html
 
 if [ -L "/data/web_static/current" ]; then
-		rm /data/web_static
+		rm /data/web_static/current
+		sudo ln -s 
