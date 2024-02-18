ca78ea6ce62e42e3ee80bed2adb910f5ffedfcba
RealTonyBlaq <ifeanyiikpenyi@yahoo.com>
2024-02-18 18:52:30 +0100

    f

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -9,8 +9,8 @@ def do_pack():
     """
     do_pack - Fab function to generate .tgz archives
     """
-    with cd('~/AirBnB_clone_v2'):
-        result = local('date +"%Y%m%d%H%M%S"')
-        local('tar -czvf web_static{}.tgz ./web_static/'.format(result))
-        local('mkdir -p versions/')
-        local('mv web_static{}.tgz versions/'.format(result))
+    local(cd '~/AirBnB_clone_v2')
+    result = local('date +"%Y%m%d%H%M%S"')
+    local('tar -czvf web_static{}.tgz ./web_static/'.format(result))
+    local('mkdir -p versions/')
+    local('mv web_static{}.tgz versions/'.format(result))
