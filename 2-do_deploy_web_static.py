c3b34b61c81776b5dbf004a4479a4e491e5d022f
RealTonyBlaq <ifeanyiikpenyi@yahoo.com>
2024-02-21 23:41:11 +0100

    arch distr

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -12,7 +12,7 @@ def do_deploy(archive_path):
     if not os.path.exists(archive_path):
         return False
     env.user = "ubuntu"
-    env.hosts = ["54.152.133.156", ]
+    env.hosts = ["54.152.133.156", "54.165.176.3"]
     filename = archive_path.split(".")[0]
     with cd("/"):
         put(archive_path, "tmp/")
