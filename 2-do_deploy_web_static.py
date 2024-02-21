16bd5985f26ea532ed519e8fc813af1975a0c623
RealTonyBlaq <ifeanyiikpenyi@yahoo.com>
2024-02-21 22:55:14 +0100

    arch distr

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,4 +1,10 @@
 #!/usr/bin/python3
 """
-Script distributes an archive to my web servers, using the function do_deploy()
+Script distributes an archive to my web servers,
+using the function do_deploy()
 """
+
+from fabric.api import run
+
+
+def do_deploy(archive_path):
