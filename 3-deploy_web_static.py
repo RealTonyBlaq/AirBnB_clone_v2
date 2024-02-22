505218eac5a7179f10ddf56e26e35e8bd79a3f91
RealTonyBlaq <ifeanyiikpenyi@yahoo.com>
2024-02-22 08:11:35 +0100

    fix

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -19,6 +19,25 @@ def do_pack():
     else:
         return "versions/{}".format(arch)
 
+def do_deploy(archive_path):
+    """ Puts an archive to my web servers and uncompresses it """
+    if not os.path.exists(archive_path):
+        return False
+    try:
+        filename = archive_path.split("/")[-1]
+        file_no_ext = filename.split('.')[0]
+        put(archive_path, "/tmp/")
+        run("mkdir -p /data/web_static/current/")
+        run("mkdir -p /data/web_static/releases/{}/".format(file_no_ext))
+        run("tar -xzf /tmp/{} -C /data/web_static/releases/{}".format(
+            filename, file_no_ext))
+        run("rm /tmp/{}".format(filename))
+        run("rm -rf /data/web_static/current")
+        run("ln -s /data/web_static/releases/{} /data/web_static/current"
+            .format(filename))
+        return True
+    except Exception:
+        return False
 def deploy():
     """ Performs the do_deploy and do_pack functions """
     archive_path = do_pack()
