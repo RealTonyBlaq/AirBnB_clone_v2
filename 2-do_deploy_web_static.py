b601cf15592671a26712ace2662ed206980b84cb
RealTonyBlaq <ifeanyiikpenyi@yahoo.com>
2024-02-22 06:25:41 +0100

    arch distr

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -8,6 +8,20 @@ from fabric.api import *
 import os
 
 
+def do_pack():
+    """
+    do_pack - Fab function to generate .tgz archives
+    """
+    time = datetime.utcnow().strftime("%Y%m%d%H%M%S")
+    arch = "web_static_{}.tgz".format(time)
+    local('mkdir -p versions/')
+    archive = local('tar -czvf versions/{} web_static'.format(arch))
+    if archive.failed:
+        return None
+    else:
+        return "versions/{}".format(arch)
+
+
 def do_deploy(archive_path):
     if not os.path.exists(archive_path):
         return False
