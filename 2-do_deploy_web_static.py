66ba3c866c5ceba64d61022d5d08c2ed2abca2be
RealTonyBlaq <ifeanyiikpenyi@yahoo.com>
2024-02-22 07:27:59 +0100

    fix

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -11,6 +11,7 @@ import os
 env.user = 'ubuntu'
 env.hosts = ['54.152.133.156', '54.165.176.3']
 
+
 def do_deploy(archive_path):
     if not os.path.exists(archive_path):
         return False
@@ -21,8 +22,8 @@ def do_deploy(archive_path):
             put(archive_path, "tmp/")
             run("mkdir -p /data/web_static/current/")
             run("mkdir -p /data/web_static/releases/{}/".format(file_no_ext))
-            run("tar -xzf /tmp/{} -C /data/web_static/releases/{}".format(filename,
-                                                                file_no_ext))
+            run("tar -xzf /tmp/{} -C /data/web_static/releases/{}".format(
+                filename, file_no_ext))
             run("rm tmp/{}".format(filename))
             run("rm -rf /data/web_static/current")
             run("ln -s /data/web_static/releases/{} /data/web_static/current"
