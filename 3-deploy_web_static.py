c250bc31d8eb73e4750d4041c3043f560c3dd509
RealTonyBlaq <ifeanyiikpenyi@yahoo.com>
2024-02-22 07:53:06 +0100

    wip

---
+++ 3-deploy_web_static.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+""" Script deploys an archive to my servers """
+
+from fabric.api import env
+do_deploy = __import__('2-do_deploy_web_static').do_deploy
+do_pack = __import__('1-pack_web_static').do_pack
+
+env.hosts = ['54.152.133.156', '54.165.176.3']
+
+
+
