43e60be65c36814e4883b6f6a87799d44796ba32
RealTonyBlaq <ifeanyiikpenyi@yahoo.com>
2024-02-22 07:33:59 +0100

    fix

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -15,17 +15,17 @@ def do_deploy(archive_path):
     if not os.path.exists(archive_path):
         return False
     try:
-        filename = archive_path.split("/")[1]
+        filename = archive_path.split("/")[-1]
         file_no_ext = filename.split('.')[0]
         put(archive_path, "/tmp/")
         run("mkdir -p /data/web_static/current/")
         run("mkdir -p /data/web_static/releases/{}/".format(file_no_ext))
         run("tar -xzf /tmp/{} -C /data/web_static/releases/{}".format(
             filename, file_no_ext))
-        run("rm tmp/{}".format(filename))
+        run("rm /tmp/{}".format(filename))
         run("rm -rf /data/web_static/current")
-            run("ln -s /data/web_static/releases/{} /data/web_static/current"
-                .format(filename))
-            return True
+        run("ln -s /data/web_static/releases/{} /data/web_static/current"
+            .format(filename))
+        return True
     except Exception:
         return False
