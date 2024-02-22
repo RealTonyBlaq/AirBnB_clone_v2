0e8996d192732a4609c09498157f40971787c754
RealTonyBlaq <ifeanyiikpenyi@yahoo.com>
2024-02-22 07:32:33 +0100

    fix

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -4,7 +4,7 @@ Script distributes an archive to my web servers,
 using the function do_deploy().
 """
 
-from fabric.api import *
+from fabric.api import put, run, env, cd
 from datetime import datetime
 import os
 
