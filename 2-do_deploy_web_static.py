f267dcbf4e0bff8d267bdae9ae8173ce156690ab
RealTonyBlaq <ifeanyiikpenyi@yahoo.com>
2024-02-22 07:27:12 +0100

    fix

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -11,21 +11,6 @@ import os
 env.user = 'ubuntu'
 env.hosts = ['54.152.133.156', '54.165.176.3']
 
-
-def do_pack():
-    """
-    do_pack - Fab function to generate .tgz archives
-    """
-    time = datetime.utcnow().strftime("%Y%m%d%H%M%S")
-    arch = "web_static_{}.tgz".format(time)
-    local('mkdir -p versions/')
-    archive = local('tar -czvf versions/{} web_static'.format(arch))
-    if archive.failed:
-        return None
-    else:
-        return "versions/{}".format(arch)
-
-
 def do_deploy(archive_path):
     if not os.path.exists(archive_path):
         return False
