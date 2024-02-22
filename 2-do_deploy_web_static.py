a5df429f2295b2bc1f9bd86135807af15a8f1147
RealTonyBlaq <ifeanyiikpenyi@yahoo.com>
2024-02-22 07:19:59 +0100

    fix

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -31,9 +31,10 @@ def do_deploy(archive_path):
         return False
     try:
         filename = archive_path.split("/")[1]
+        file_no
         with cd("/"):
             put(archive_path, "tmp/")
-            run("mkdir -p /data/web_static/releases/{}".format())
+            run("mkdir -p /data/web_static/releases/{}".format()
             run("tar -xzf /tmp/{} -C /data/web_static/releases/{}".format(filename,
                                                                 filename.split('.')[0]))
             run("rm tmp/{}".format(filename))
