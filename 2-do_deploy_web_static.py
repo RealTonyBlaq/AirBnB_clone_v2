e4e7f6a13d0c9a7eb1e353f68f9327c2dcfb4eb0
RealTonyBlaq <ifeanyiikpenyi@yahoo.com>
2024-02-22 07:33:16 +0100

    fix

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -4,7 +4,7 @@ Script distributes an archive to my web servers,
 using the function do_deploy().
 """
 
-from fabric.api import put, run, env, cd
+from fabric.api import put, run, env
 from datetime import datetime
 import os
 
@@ -17,14 +17,13 @@ def do_deploy(archive_path):
     try:
         filename = archive_path.split("/")[1]
         file_no_ext = filename.split('.')[0]
-        with cd("/"):
-            put(archive_path, "tmp/")
-            run("mkdir -p /data/web_static/current/")
-            run("mkdir -p /data/web_static/releases/{}/".format(file_no_ext))
-            run("tar -xzf /tmp/{} -C /data/web_static/releases/{}".format(
-                filename, file_no_ext))
-            run("rm tmp/{}".format(filename))
-            run("rm -rf /data/web_static/current")
+        put(archive_path, "/tmp/")
+        run("mkdir -p /data/web_static/current/")
+        run("mkdir -p /data/web_static/releases/{}/".format(file_no_ext))
+        run("tar -xzf /tmp/{} -C /data/web_static/releases/{}".format(
+            filename, file_no_ext))
+        run("rm tmp/{}".format(filename))
+        run("rm -rf /data/web_static/current")
             run("ln -s /data/web_static/releases/{} /data/web_static/current"
                 .format(filename))
             return True
