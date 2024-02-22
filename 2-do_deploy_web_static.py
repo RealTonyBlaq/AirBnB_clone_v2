f153ea124fd1c08b5e208093aa8a0c986451950b
RealTonyBlaq <ifeanyiikpenyi@yahoo.com>
2024-02-22 08:20:46 +0100

    fixx

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -25,7 +25,7 @@ def do_deploy(archive_path):
         run("rm /tmp/{}".format(filename))
         run("rm -rf /data/web_static/current")
         run("ln -s /data/web_static/releases/{} /data/web_static/current"
-            .format(filename))
+            .format(file_no_ext))
         return True
     except Exception:
         return False
