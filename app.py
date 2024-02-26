c87813b92a508b837eff8342d0b22ca9f2bdf63b
RealTonyBlaq <ifeanyiikpenyi@yahoo.com>
2024-02-26 18:48:48 +0100

    test

---
+++ app.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+""" Testy script """
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+tasks = [
+    {
+        'id': 1,
+        'title': u'Buy groceries',
+        'description': u'Milk, Cheese, Pizza, Fruit, Tylenol', 
+        'done': False
+    },
+    {
+        'id': 2,
+        'title': u'Learn Python',
+        'description': u'Need to find a good Python tutorial on the web', 
+        'done': False
+    }
+]
