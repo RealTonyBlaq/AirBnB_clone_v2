65cfbd94d430917e967b2bea1cd3d42d13680639
RealTonyBlaq <ifeanyiikpenyi@yahoo.com>
2024-03-17 08:52:55 +0100

    added reload()

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -63,3 +63,7 @@ class FileStorage:
                     self.all()[key] = classes[val['__class__']](**val)
         except FileNotFoundError:
             pass
+
+    def close(self):
+        """ Calls reload() to deserialize objects """
+        self.reload()
