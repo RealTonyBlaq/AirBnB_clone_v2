0f383e22f78b0bb77057023d6ef72d53c6783947
RealTonyBlaq <ifeanyiikpenyi@yahoo.com>
2024-03-17 08:53:08 +0100

    added close()

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -73,3 +73,7 @@ class DBStorage:
         sess = sessionmaker(bind=self.__engine, expire_on_commit=False)
         Session = scoped_session(sess)
         self.__session = Session()
+
+    def close(self):
+        """ Calls self.remove() """
+        self.__session.remove()
