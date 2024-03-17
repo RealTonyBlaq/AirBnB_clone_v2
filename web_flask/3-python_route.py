d05acb5a3ed4a5d84bf32b9323b89680040573b9
RealTonyBlaq <ifeanyiikpenyi@yahoo.com>
2024-03-17 07:04:11 +0100

    more routes to 2-c_route.py

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+""" Script starts a Web Flask App """
+
+from flask import Flask
+
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def root():
+    """ Returns a string """
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb', strict_slashes=False)
+def sub():
+    """ Returns a string when this path is accessed """
+    return "HBNB"
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def c_route(text):
+    """ Returns a string + text passed to the url """
+    if "_" in text:
+        text = text.replace('_', ' ')
+    return "C {}".format(text)
+
+
+if __name__ == "__main__":
+    app.run("0.0.0.0", 5000)
