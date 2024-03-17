24abff590b06cc03e14f36713ef273d1717e7166
RealTonyBlaq <ifeanyiikpenyi@yahoo.com>
2024-03-17 06:57:58 +0100

    added another route

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+""" Script starts a Web Flask App """
+
+from flask import Flask
+
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def root():
+    """ Returns a string """
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb', strict_slashes=False)
+def sub():
+    """ Returns a string when this path is accessed """
+    return "HBNB"
+
+
+@app.
+
+
+if __name__ == "__main__":
+    app.run("0.0.0.0", 5000)
