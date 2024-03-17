b29f1018c2ae05cd9cff8d20b548072951359f77
RealTonyBlaq <ifeanyiikpenyi@yahoo.com>
2024-03-17 06:48:23 +0100

    a Flask web application

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -10,4 +10,8 @@ app = Flask(__name__)
 @app.route('/', strict_slashes=False)
 def root():
     """ Returns a string """
-    return “Hello HBNB!”
+    return "Hello HBNB!"
+
+
+if __name__ == "__main__":
+    
