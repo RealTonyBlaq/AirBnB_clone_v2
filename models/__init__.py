1b9ad3111ddc24c1bb9796469efb4a06107dbbf1
RealTonyBlaq <ifeanyiikpenyi@yahoo.com>
2024-03-17 08:56:25 +0100

    update

--- models/__init__.py
+++ models/__init__.py
@@ -8,7 +8,9 @@ from models.amenity import Amenity
 from models.review import Review
 from os import getenv
 
-if getenv("HBNB_TYPE_STORAGE") == "db":
+
+storage_type = getenv("HBNB_TYPE_STORAGE")
+if storage_type == "db":
     from models.engine.db_storage import DBStorage
     storage = DBStorage()
     storage.reload()
