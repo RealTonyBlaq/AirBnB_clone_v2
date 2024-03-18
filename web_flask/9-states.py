367a01ad05ad1c5132fe8758b23fe0a30f567230
RealTonyBlaq <ifeanyiikpenyi@yahoo.com>
2024-03-18 21:44:12 +0100

    new route

--- web_flask/9-states.py
+++ web_flask/9-states.py
@@ -19,7 +19,7 @@ def teardown(error=None):
 @app.route('/states', strict_slashes=False)
 def city_by_states():
     """ Returns a rendered list of cities by state objects """
-    states = storage.all()
+    states = [v.storage.all(State)
     
 
 if __name__ == "__main__":
