2da19d7bb6d156bdaaa339e63e07afdb463d10e0
RealTonyBlaq <ifeanyiikpenyi@yahoo.com>
2024-03-18 07:14:32 +0100

    fix pycodestyle

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -20,7 +20,7 @@ def all_states():
     """ Returns a list of state objects """
     states = storage.all(State)
     state_list = [v.to_dict() for v in states.values()]
-    sorted_list = sorted(state_list, key=lambda x: x['name']) 
+    sorted_list = sorted(state_list, key=lambda x: x['name'])
     render = render_template('7-states_list.html', state_list=sorted_list)
     return render
 
