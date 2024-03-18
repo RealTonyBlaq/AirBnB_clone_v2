74fa3af2503d8a158e89e08d22f153e98c7d9077
RealTonyBlaq <ifeanyiikpenyi@yahoo.com>
2024-03-18 21:58:40 +0100

    new route

--- web_flask/9-states.py
+++ web_flask/9-states.py
@@ -20,7 +20,7 @@ def teardown(error=None):
 @app.route('/states/<id>', strict_slashes=False)
 def city_by_states(id):
     """ Returns a rendered list of cities by state objects """
-    
+    if not id:
     states = [v.to_dict() for v in storage.all(State).values()]
     sorted_states = sorted(states, key=lambda x: x['name'])
     return render_template('9-states.html', states_list=sorted_states)
