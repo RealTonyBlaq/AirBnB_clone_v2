97d2613ec86a7948accb8ee850af62e1b2e017e1
RealTonyBlaq <ifeanyiikpenyi@yahoo.com>
2024-03-18 19:00:13 +0100

    city by state

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -23,7 +23,7 @@ def all_states():
     cities = [v.to_dict() for v in storage.all(City).values()]
     sorted_states = sorted(states, key=lambda x: x['name'])
     sorted_cities = sorted(cities, key=lambda x: x['name'])
-    for 
+    for state in sorted_states()
     render = render_template('7-states_list.html', state_list=sorted_list)
     return render
 
