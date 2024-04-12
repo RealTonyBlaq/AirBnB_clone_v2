f05492ee1801422a7b790bfa3332e9376b89f137
RealTonyBlaq <ifeanyiikpenyi@yahoo.com>
2024-04-12 23:31:18 +0100

    puppet

--- 101-setup_web_static.pp
+++ 101-setup_web_static.pp
@@ -36,7 +36,9 @@ file {'/data/':
 }
 
 exec {'serve_static':
-    command => 'sed -i "48i\        location /hbnb_static {        alias /data/web_static/current/;}" /etc/nginx/sites-available/default'
+    command => 'sed -i "48i\        location /hbnb_static {        alias /data/web_static/current/;}" /etc/nginx/sites-available/default',
+    path    => '/usr/bin/',
+    require =>
 }
 
 service {'nginx':
