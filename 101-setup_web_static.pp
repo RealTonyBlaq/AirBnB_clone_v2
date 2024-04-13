1ef433f01c35d93e38075b50bc2e0cb56d2f2f25
RealTonyBlaq <ifeanyiikpenyi@yahoo.com>
2024-04-13 22:06:55 +0100

    fix

--- 101-setup_web_static.pp
+++ 101-setup_web_static.pp
@@ -1,30 +1,30 @@
 # Puppet installs Nginx and sets up your web servers for the deployment of web_static
 
 package {'nginx':
-    ensure => 'installed'
+    ensure => 'installed',
 }
 
 exec {'make_rel':
     command => 'mkdir -p /data/web_static/releases/test/',
-    path    => '/usr/bin/'
+    path    => '/usr/bin/',
 }
 
 exec {'make_shared':
     command => 'mkdir -p /data/web_static/shared/',
-    path    => '/usr/bin/'
+    path    => '/usr/bin/',
 }
 
 file {'/data/web_static/releases/test/index.html':
     ensure  => 'present',
     mode    => '0644',
     content => 'Testing my Nginx configuration',
-    require => Exec['make_rel']
+    require => Exec['make_rel'],
 }
 
 exec {'sym_link':
     command => 'ln -s /data/web_static/releases/test/ /data/web_static/current',
     path    => '/usr/bin/',
-    require => Exec['make_rel']
+    require => Exec['make_rel'],
 }
 
 file {'/data/':
@@ -32,16 +32,17 @@ file {'/data/':
     owner   => 'ubuntu',
     group   => 'ubuntu',
     recurse => true,
-    require => Exec['make_rel', 'make_shared']
+    require => Exec['make_rel'],
 }
 
 exec {'serve_static':
     command => 'sed -i "48i\        location /hbnb_static {        alias /data/web_static/current/;}" /etc/nginx/sites-available/default',
     path    => '/usr/bin/',
-    require => Package['nginx']
+    require => Package['nginx'],
 }
 
 service {'nginx':
     ensure  => 'running',
-    require => Package['nginx']
+    enable  => true,
+    require => Package['nginx'],
 }
